14bab2681ee301910451ae7dd03bd2fb0375b069
tshelto17 <tim.shelton17@t-mobile.com>
2011-12-06 13:49:56 -0800

    another change

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, world!");
-            
+            Console.WriteLine("Hi!");
             Console.Beep();
         }
     }
